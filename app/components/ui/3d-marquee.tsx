594c5c552ac24ff2704abb283ae54101f2baf564
SyedAliHassanNaqvi <142111910+SyedAliHassanNaqvi@users.noreply.github.com>
2026-08-28 19:13:09 +0500

    Added Zoomed in effect in 3D Marquee Card for mobile devices

--- app/components/ui/3d-marquee.tsx
+++ app/components/ui/3d-marquee.tsx
@@ -47,11 +47,11 @@ export const ThreeDMarquee = ({ images, className }: ThreeDMarqueeProps) => {
 
   return (
     <div
-  className={`mx-auto block w-full h-[600px] overflow-hidden rounded-2xl max-sm:h-[500px] ${className ?? ""}`}
+  className={`mx-auto block w-full h-[600px] overflow-hidden rounded-2xl max-sm:h-[400px] ${className ?? ""}`}
   style={{ perspective: "1200px" }}
 >
       <div className="flex size-full items-center justify-center">
-        <div className="relative size-[1720px] shrink-0 scale-[0.4] sm:scale-[0.65] lg:scale-100">
+        <div className="relative size-[1720px] shrink-0 scale-[0.5] max-sm:-mt-8 sm:scale-[0.65] lg:scale-100">
           <div
             style={{
               transform: "translate(-50%, -50%) rotateX(55deg) rotateZ(-45deg)",
